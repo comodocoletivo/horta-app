c318e775cf26ddac8c056e393d78c7e138606086
thulioph <thulioph@gmail.com>
2016-05-31 04:17:03 -0300

    Adiciona actionsheet para deletar item da horta.

--- www/js/controllers/profile.js
+++ www/js/controllers/profile.js
@@ -6,13 +6,33 @@
     .module('starter.controllers')
     .controller('ProfileCtrl', Profile);
 
-    Profile.$inject = ['$scope'];
+    Profile.$inject = ['$scope', '$ionicActionSheet'];
 
-    function Profile($scope) {
+    function Profile($scope, $ionicActionSheet) {
       /* jshint validthis: true */
       var vm = this;
 
-      console.warn('ProfileCtrl');
+      vm.action = showAction;
+
+      function showAction(id) {
+        var id, hideSheet;
+
+        id = id;
+
+       hideSheet = $ionicActionSheet.show({
+         buttons: [{ text: 'Sim' }],
+         titleText: 'Remover item da horta',
+         cancelText: 'Cancel',
+         cancel: function() {
+          hideSheet();
+        },
+        buttonClicked: function(index) {
+          console.log('id foi', id)
+         return true;
+       }
+
+     });
+      }
     }
 
 })();
