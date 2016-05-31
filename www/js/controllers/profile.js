7b5315f5d6bae7010491e936cb2ad4c8f9d06f6a
thulioph <thulioph@gmail.com>
2016-05-31 04:36:19 -0300

    Ajusta tela de login e fluxo para adicionar novo produto.

--- www/js/controllers/profile.js
+++ www/js/controllers/profile.js
@@ -6,33 +6,50 @@
     .module('starter.controllers')
     .controller('ProfileCtrl', Profile);
 
-    Profile.$inject = ['$scope', '$ionicActionSheet'];
+    Profile.$inject = ['$scope', '$ionicActionSheet', '$ionicModal'];
 
-    function Profile($scope, $ionicActionSheet) {
+    function Profile($scope, $ionicActionSheet, $ionicModal) {
       /* jshint validthis: true */
       var vm = this;
 
       vm.action = showAction;
+      vm.modal = showModal;
+      vm.addItem = addItem;
 
       function showAction(id) {
         var id, hideSheet;
 
         id = id;
 
-       hideSheet = $ionicActionSheet.show({
-         buttons: [{ text: 'Sim' }],
-         titleText: 'Remover item da horta',
-         cancelText: 'Cancel',
-         cancel: function() {
-          hideSheet();
-        },
-        buttonClicked: function(index) {
-          console.log('id foi', id)
-         return true;
-       }
+        hideSheet = $ionicActionSheet.show({
+           buttons: [{ text: 'Sim' }],
+           titleText: 'Remover item da horta',
+           cancelText: 'Cancel',
+           cancel: function() {
+            hideSheet();
+          },
+          buttonClicked: function(index) {
+            console.log('id foi', id)
+            return true;
+          }
 
-     });
+        });
       }
+
+      function showModal() {
+        $ionicModal.fromTemplateUrl('templates/modals/add-item.html', {
+          scope: $scope,
+          animation: 'slide-in-up'
+        }).then(function(modal) {
+          $scope.modal = modal;
+          $scope.modal.show();
+        });
+      }
+
+      function addItem() {
+        console.log('hey you');
+      }
+
     }
 
 })();
