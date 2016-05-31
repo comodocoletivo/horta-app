22c23f6bd0542cfe4af91a8bb0ca79031e7b1abc
thulioph <thulioph@gmail.com>
2016-05-31 04:01:49 -0300

    Ajusta fluxo para login por e-mail.

--- www/js/controllers/login.js
+++ www/js/controllers/login.js
@@ -6,14 +6,16 @@
     .module('starter.controllers')
     .controller('LoginCtrl', Login);
 
-    Login.$inject = ['$scope', '$log', 'Loginservice', '$location'];
+    Login.$inject = ['$scope', '$log', 'Loginservice', '$location', '$ionicModal'];
 
-    function Login($scope, $log, Loginservice, $location) {
+    function Login($scope, $log, Loginservice, $location, $ionicModal) {
       /* jshint validthis: true */
       var vm = this;
 
       vm.loginData = {};
       vm.submit = submitForm;
+      vm.modal = openModal;
+      vm.authFb = authFb;
 
       function submitForm() {
         return submit().then(function() {
@@ -27,12 +29,35 @@
         var params = vm.loginData;
 
         return Loginservice.authEmail(params).then(function(result) {
-          $location.path('app/map');
+          $scope.modal.hide()
           $log.info('Loginservice.authEmail: ', result);
+          $location.path('app/map');
         }, function(err) {
           if (err === 401) { console.log('não tem permissão') }
           else {$log.warn('status error: ', err)}
         })
+      }
+
+      function openModal(args) {
+        var params = args;
+
+        if (params === 'email') {
+          emailModal();
+        }
+      }
+
+      function emailModal() {
+        $ionicModal.fromTemplateUrl('templates/modals/login-email.html', {
+          scope: $scope,
+          animation: 'slide-in-up'
+        }).then(function(modal) {
+          $scope.modal = modal;
+          $scope.modal.show();
+        });
+      }
+
+      function authFb() {
+        console.log('login por facebook')
       }
 
 
