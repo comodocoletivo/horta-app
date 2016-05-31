a5215fd854d962586de108d30aa2edb6f2e35ca6
thulioph <thulioph@gmail.com>
2016-05-31 05:58:08 -0300

    Exibe todos os favoritos.

--- www/js/controllers/favorites.js
+++ www/js/controllers/favorites.js
@@ -6,13 +6,33 @@
     .module('starter.controllers')
     .controller('FavoritesCtrl', Favorites);
 
-    Favorites.$inject = ['$scope'];
+    Favorites.$inject = ['$scope', 'UserApi', '$log'];
 
-    function Favorites($scope) {
+    function Favorites($scope, UserApi, $log) {
       /* jshint validthis: true */
       var vm = this;
 
-      console.warn('FavoritesCtrl');
+
+      // vm.getAll = getAll;
+      getAll();
+
+      function getAll() {
+        return get().then(function() {
+        });
+      }
+
+      function get() {
+        return UserApi.getAllFavorites().then(function(result) {
+          $log.info('getAllFavorites: ', result.favorites);
+          vm.all_favorites = result.favorites;
+        }, function(err) {
+          if (err === 401) { console.log('não tem permissão') }
+          else {$log.warn('status error: ', err)}
+        })
+      }
+
+
+
     }
 
 })();
--- www/js/services/user.js
+++ www/js/services/user.js
@@ -14,11 +14,45 @@
       apiUrl = ApiConfig.API_URL;
 
       return {
-        lookup: lookup
+        lookup: lookup,
+        getAllFavorites: getAllFavorites,
+        addFavorite: addFavorite
       };
 
       function lookup() {
         return $http.get(apiUrl + '/api/v1/me/',{ headers: {
+          'Content-Type': 'application/json',
+          'Authorization': sessionStorage.getItem('authorization')
+        }}).then(success).catch(error);
+
+        function success(response) {
+          return response.data;
+        }
+
+        function error(err) {
+          return $q.reject(err.status);
+        }
+      }
+
+      function getAllFavorites() {
+        return $http.get(apiUrl + '/api/v1/favorite/',{
+          headers: {
+          'Content-Type': 'application/json',
+          'Authorization': sessionStorage.getItem('authorization')
+        }}).then(success).catch(error);
+
+        function success(response) {
+          return response.data;
+        }
+
+        function error(err) {
+          return $q.reject(err.status);
+        }
+      }
+
+      function addFavorite(data) {
+        return $http.post(apiUrl + '/api/v1/favorite/', data, {
+          headers: {
           'Content-Type': 'application/json',
           'Authorization': sessionStorage.getItem('authorization')
         }}).then(success).catch(error);
